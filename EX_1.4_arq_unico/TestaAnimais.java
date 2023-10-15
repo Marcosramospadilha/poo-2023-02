f436ff2e32064982b1c0869cd76d110517658318
Marcos <eaufg.marcos@gmail.com>
2023-10-15 13:23:38 -0300

    a

---
+++ EX_1.4_arq_unico/TestaAnimais.java
@@ -0,0 +1,36 @@
+class Animal {
+    public String som() {
+        return "Som produzido pelo animal";
+    }
+}
+
+class Sapo extends Animal {
+    public String som() {
+        return "coaxar";
+    }
+}
+
+class Cachorro extends Animal {
+    public String som() {
+        return "latir";
+    }
+}
+
+class Boi extends Animal {
+    public String som() {
+        return "berrar";
+    }
+}
+
+public class TestaAnimais {
+    public static void main(String[] args) {
+        Animal[] animais = new Animal[3];
+        animais[0] = new Sapo();
+        animais[1] = new Cachorro();
+        animais[2] = new Boi();
+
+        for (int i = 0; i < animais.length; i++) {
+            System.out.println("Animal " + (i + 1) + ": " + animais[i].som());
+        }
+    }
+}
