93c2eaf6d0dc52be875ce88fb49c4d6d61f591bd
Marcos <eaufg.marcos@gmail.com>
2023-11-19 19:26:24 -0300

    a

--- EX_1.9/Principal.java
+++ EX_1.9/Principal.java
@@ -3,26 +3,24 @@ import javax.swing.JOptionPane;
 
 public class Principal {
     public static void main(String[] args) {
-        DecimalFormat dc = new DecimalFormat("0,00");
+        DecimalFormat dc = new DecimalFormat("0.00");
         Calc c = new Calc();
-            try{        
-                c.n1 = Integer.parseInt(JOptionPane.showInputDialog(null,
-                        "Entre com o primeiro número:"));
-                c.n2 = Integer.parseInt(JOptionPane.showInputDialog(null,
-                        "Entre com o segundo número:"));
-            }catch()
-                JOptionPane.showMessageDialog(null,
-                        "Soma : " + dc.format(c.somar()));
-                JOptionPane.showMessageDialog(null,
 
-                        "Subtração : " + dc.format(c.diminuir()));
+        try {
+            c.n1 = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o primeiro número:"));
+            c.n2 = Integer.parseInt(JOptionPane.showInputDialog(null, "Entre com o segundo número:"));
 
-                JOptionPane.showMessageDialog(null,
+            JOptionPane.showMessageDialog(null, "Soma : " + dc.format(c.somar()));
+            JOptionPane.showMessageDialog(null, "Subtração : " + dc.format(c.diminuir()));
+            JOptionPane.showMessageDialog(null, "Multiplicação : " + dc.format(c.multiplicar()));
+            JOptionPane.showMessageDialog(null, "Divisão : " + dc.format(c.dividir()));
 
-                        "Multiplicação : " + dc.format(c.multiplicar()));
-
-                JOptionPane.showMessageDialog(null,
-                        "Divisão : " + dc.format(c.dividir()));
-
+        } catch (NumberFormatException e) {
+            JOptionPane.showMessageDialog(null, "Erro: Valor inválido. Por favor, insira um número válido.");
+        } catch (ArithmeticException e) {
+            JOptionPane.showMessageDialog(null, "Erro: Divisão por zero.");
+        } catch (NumeroNegativo e) {
+            JOptionPane.showMessageDialog(null, "Erro: Número negativo inserido.");
+        }
     }
 }
